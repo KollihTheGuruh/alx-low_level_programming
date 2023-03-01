83baead9d69d5fef4935728bd0670def7660ea5b
KollihTheGuruh <mmike8449@gmail.com>
2023-03-01 13:07:16 -0800

    numbers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-/* main - prints a[2] = 98, followed by a new line. */
-
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+	int n;
+	int a[5];
+	int *p;
 
-a[2] = 1024;
-p = &n;
+	a[2] = 1024;
+	p = &n;
 	/*
 	* write your line of code here...
 	* Remember:
