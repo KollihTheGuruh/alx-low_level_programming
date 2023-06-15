80d840833496544926acc21444046cb85eb05a73
KollihTheGuruh <mmike8449@gmail.com>
2023-06-15 13:32:47 -0700

    100-result

--- 0x17-doubly_linked_lists/102-result.c
+++ 0x17-doubly_linked_lists/102-result.c
@@ -1,46 +1,26 @@
 #include <stdio.h>
-/**
- * main - find palindrome of largest product of two 3-digit numbers
- * Return: Always 1
- */
-int main(void)
-{
-	int product, max = 0, i, j, c = 0;
-	int numDigits = 6;
-	int d[6] = { 0 };
 
-	for (i = 999; i > 100; i--)
-	{
-		for (j = 999; j > 100; j--)
-		{
-			product = i * j;
-			c = 0;
-			d[0] = d[1] = d[2] = d[3] = d[4] = d[5] = 0;
-			while (product)
-			{
-				d[c] = product % 10;
-				product /= 10;
-				c++;
-			}
-			product = i * j;
-			numDigits = c;
-			if (numDigits == 6)
-			{
-				if ((d[0] == d[5]) &&
-				    (d[1] == d[4]) &&
-				    (d[2] == d[3]))
-					if (product > max)
-						max = product;
-			}
-			else if (numDigits == 5)
-			{
-				if ((d[0] == d[4]) &&
-				    (d[1] == d[3]))
-					if (product > max)
-						max = product;
-			}
-		}
-	}
-	printf("%d\n", max);
-	return (1);
+int is_palindrome(int n) {
+int rev = 0;
+int temp = n;
+while (temp != 0) {
+rev = rev * 10 + temp % 10;
+temp /= 10;
+}
+return n == rev;
+}
+
+int main() {
+int i, j;
+int largest = 0;
+for (i = 100; i < 1000; i++) {
+for (j = i; j < 1000; j++) {
+int product = i * j;
+if (is_palindrome(product) && product > largest) {
+largest = product;
+}
+}
+}
+printf("%d", largest);
+return 0;
 }
